0c3d2ae808e15b41c5446c72ceee5e22392a2cb6
lppellin <thelucaspellin@gmail.com>
2024-08-08 20:28:39 -0300

    script, implementado as funções dos exercicios

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("Script funcionando")
+
+function soma(a, b) {
+    return a + b
+}
+
+function gerarResultado() {
+    let numero1 = parseFloat(document.getElementById("num1").value);
+    let numero2 = parseFloat(document.getElementById("num2").value);
+    alert("calculando...")
+
+    const resultado = soma(numero1, numero2)
+
+    alert("O resultado é: " + resultado)
+
+    document.getElementById("txt-resultado").innerText = "Resultado: " + resultado
+}
